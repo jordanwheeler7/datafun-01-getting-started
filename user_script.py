4530dbf6b8be6e76ef43d3e462f2764ad0c1f32d
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-01-15 20:57:06 -0500

    Final Copy

--- user_script.py
+++ user_script.py
@@ -1,20 +1,20 @@
 """
 Complete the script.
 """
-from decimal import Decimal
+
 
 
 print("Hello!")
 print()
 print("Enter your latest three run times")
 print()
-run_time1 = int(input("What was your latest run time?"))
-run_time2 = int(input("What was the second to latest run time?"))
-run_time3 = int(input("What was your third latest run time?"))
+run_time1 = float(input("What was your latest run time?"))
+run_time2 = float(input("What was the second to latest run time?"))
+run_time3 = float(input("What was your third latest run time?"))
 print()
 run_timesum = int(run_time1 + run_time2 + run_time3)
 run_timeaverage = int(run_timesum / 3)
-
+# Use this information to perform calculations for the total and average run time
 
 print("We will see our stats for our run times")
 print()
@@ -26,6 +26,7 @@ print()
 print("Total time spent running")
 print(run_timesum)
 print()
+# This section will allow us to see the minimum and maximum run times
 min = run_time1
 if run_time2 < run_time1:
     min = run_time2
@@ -43,11 +44,35 @@ if run_time3 > run_time1:
 print( "Your slowest run was") 
 print(max)
 
+# This area allows us to see the ranage of times
+
 print("Our runs ranged in times from")
-print('Range:', (min), '-', (max))
+print('Range:', int(min), '-', int(max))
+
+# This section shows the adjustment for an increase in elevation of 1,000 feet
 
 print("Below we will see our times adjusted for running at 1,000 ft higher elevation")
 
-print(round(int(run_time1 * (1 + Decimal(.4)))))
-print(round(int(run_time2 * (1 + Decimal(.4)))))
-print(round(int(run_time3 * (1 + Decimal(.4)))))
+print(round(float(run_time1 * (1 + float(.4)))))
+print(round(float(run_time2 * (1 + float(.4)))))
+print(round(float(run_time3 * (1 + float(.4)))))
+
+print()
+print()
+# The variables below are the fastest, slowest, and average time to run 1 mile based off a sampling. We will compare our minimum:
+# maximum and average to see where we stand as a runner
+
+x = 5
+y = 8
+z = 12
+
+# This section is to compare user run times to those of :
+# a case study of other runners in the mile
+
+if min < x :
+    print( min, "Man, you are fast!")
+if run_timeaverage == y :
+    print(run_timeaverage, "You are average")
+if max > z :
+    print(max, "Keep working! You are slower than the study")
+
--- xtra_p1.py
+++ xtra_p1.py
@@ -2,31 +2,43 @@
 Optional bonus. See course site for details.
 
 """
-
-import random
+"""
+    Jordan Wheeler 
+    15 January 2023
+    This module is similar to rock paper scissors but with animals.
+    Users will input their choice of animal and the computer will use a
+    random generator. The inputs will compare to see who wins.
+    """
 
 # Change the name below to a name of your choice
 
-name = "GameBot"
+import random
+name = "Jeph"
 
 # Fix the code below to print the name using an f-string
 
 print()
-print("Hello, I'm {name}, your gamebot.")
-print("Let's play an animal guessing game!")
-print("There are 3 animals: wolf, eagle, snake (a Python of course).")
-print("The wolf scares the eagle.")
-print("The eagle grabs the snake.")
-print("The snake bites the wolf.")
-print("I'll pick one and you pick one and we'll see who wins.")
+print(f"Hello, I'm {name}, your gamebot.")
+print(f"Let's play an animal guessing game!")
+print(f"There are 3 animals: wolf, eagle, snake (a Python of course).")
+print(f"The wolf scares the eagle.")
+print(f"The eagle grabs the snake.")
+print(f"The snake bites the wolf.")
+print(f"I'll pick one and you pick one and we'll see who wins.")
 print()
+x = "wolf"
+y = "eagle"
+z = "snake"
+
+# Winning order
+winning_order = x > y, y > z, z > x
 
 # Right now, the user choses wolf everytime.
 # Modify the code so the user is asked to
 # enter wolf, eagle, or snake.
 # Hint: use the input() function
-
-user_choice = "wolf"
+print("Pick from list")
+user_choice = input({"wolf", "eagle", "snake"})
 
 # Now the bot will pick one
 buddy_choice = random.choice(["wolf", "eagle", "snake"])
@@ -45,10 +57,22 @@ print()
 if user_choice == buddy_choice:
     print("We tied!")
 
+if user_choice == x and buddy_choice == y:
+    print("You Win!")
+if user_choice == y and buddy_choice == z:
+    print("You Win!")
+if user_choice == z and buddy_choice == x:
+    print("You Win!")
+if user_choice == y and buddy_choice == x:
+    print("I Win!")
+if user_choice == z and buddy_choice == y:
+    print("I Win!")
+if user_choice == x and buddy_choice == z:
+    print("I Win!")
 
 # When you finish,
 # right-click on the code and select "Format Document"
 
 # Run the code, and play the game a few times.
-# Copy the output from the terminal and paste it into 
+# Copy the output from the terminal and paste it into
 # a new file named xtra_p1out.txt.
